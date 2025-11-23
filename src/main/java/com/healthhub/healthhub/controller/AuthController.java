5551bdd8a85df683d78e2dcf4eaaab061a06354b
Mohamed <cheikhimohamed51@gmail.com>
2025-11-23 23:46:55 +0100

    added cross origins to the authController

--- src/main/java/com/healthhub/healthhub/controller/AuthController.java
+++ src/main/java/com/healthhub/healthhub/controller/AuthController.java
@@ -6,13 +6,11 @@ import com.healthhub.healthhub.dto.RegisterDTO;
 import com.healthhub.healthhub.service.UtilisateurService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:5173/")
 public class AuthController {
     @Autowired
     private UtilisateurService utilisateurService;
